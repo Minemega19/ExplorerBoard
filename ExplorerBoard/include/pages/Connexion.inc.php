1400c1d4879844aff1e8c7ed427ce42097aaf12a
Valentin Turpault <38724294+polurax@users.noreply.github.com>
2018-12-07 06:42:02 +0100

    Add files via upload

--- ExplorerBoard/include/pages/Connexion.inc.php
+++ ExplorerBoard/include/pages/Connexion.inc.php
@@ -1,24 +1,10 @@
-<?php
-$db = mysqli_connect("localhost","bd","bede");
-mysqli_select_db($db,"explorerboard");
-
-$req = "SELECT login, password from scientifique ";
-$result = mysqli_query($db,$req);
-$ligne=mysqli_fetch_array($result);
-
-
-
-
-
-?>
-
 
 <!DOCTYPE html>
 
 <html>
 
   <head>
-    
+
     <meta charset="utf-8" />
 	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo.png">
@@ -27,21 +13,17 @@ $ligne=mysqli_fetch_array($result);
   </head>
 
   <body>
-  	
+
   	<div id="blocMid">
 
 		<img id="logo" src="../../assets/img/logo.png">
-		
-		<input type="text" name="nom" id="champsNom" placeholder="Nom" required/>
-			
-		<input type="password" name="pwd" id="champsPwd" placeholder="password" required/>
 
-		<div id="lebouton"><a id="bouton" href="../../dashboard.html">Envoyer</a></div>
-	
-	</div>	
-	
+		<input type="text" name="nom" id="champsNom" placeholder="Nom" required/>
+
+		<input type="text" name="pre" id="champsPre" placeholder="prenom" required/>
+
+	</div>
+	<script src="../../assets/js/connexion.js"></script>
   </body>
 
 </html>
-	
-
