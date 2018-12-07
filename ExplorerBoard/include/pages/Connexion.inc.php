d31f0f99d3a80600f9026578b229379290ee7735
AxelGUZA <38752522+AxelGUZA@users.noreply.github.com>
2018-12-07 03:28:22 +0100

    connexion inc

---
+++ ExplorerBoard/include/pages/Connexion.inc.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+  <head>
+    
+    <meta charset="utf-8" />
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/logo.png">
+
+	<title>ExplorerBoard</title>
+  </head>
+
+  <body>
+  
+
+		<img src="../../assets/img/logo.png">
+		
+		<input type="text" name="nom" id="champsNom"required/>
+			
+		<input type="password" name="pwd" id="champsPwd" required/>
+		
+	
+  </body>
+
+</html>
+	
+
