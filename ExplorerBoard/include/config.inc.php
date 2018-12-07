98c5d9975fa816f2d6f5f4359cb49e81515b0263
sakires <baptistedeschampsiut@gmail.com>
2018-12-07 03:16:01 +0100

    rajout de parametrage

--- ExplorerBoard/include/config.inc.php
+++ ExplorerBoard/include/config.inc.php
@@ -1,9 +1,9 @@
 <?php
-// Paramètres de l'application Covoiturage
+// Paramï¿½tres de l'application Covoiturage
 // A modifier en fonction de la configuration
 
 define('DBHOST', "localhost");
-define('DBNAME', "covoit");
+define('DBNAME', "explorerboard");
 define('DBUSER', "bd");
 define('DBPASSWD', "bede");
 define('ENV','dev');
@@ -11,7 +11,7 @@ define('SALT','48@!alsd');
 define('DBPORT',3306);
 
 $db =mysqli_connect ("localhost","bd","bede");
-mysqli_select_db($db,"CLIENT");
-// pour un environememnt de production remplacer 'dev' (développement) par 'prod' (production)
+mysqli_select_db($db,"explorerboard");
+// pour un environememnt de production remplacer 'dev' (dï¿½veloppement) par 'prod' (production)
 // les messages d'erreur du SGBD s'affichent dans l'environememnt dev mais pas en prod
 ?>
